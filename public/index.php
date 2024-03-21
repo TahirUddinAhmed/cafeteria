22f05adba4c4165e6a2e471ae43dfc7b68d9d97d
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2024-03-21 16:31:32 +0530

    admin view

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@ $routes = [
     '/' => 'controllers/home.php',
     '/contact' => 'controllers/contact.php',
     '/booking/create' => 'controllers/bookings/create.php',
+    '/admin' => 'controllers/admin/home.php',
     '404' => 'controllers/error/404.php'
 ];
 
---
+++ view/admin/home.view.php
@@ -0,0 +1,53 @@
+<?php
+ loadPartition('admin-header');
+ loadPartition('admin-sidebar');
+?>
+
+    <main id="main" class="main">
+      <div class="pagetitle">
+        <h1>Dashboard</h1>
+        <nav>
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+            <li class="breadcrumb-item active">Dashboard</li>
+          </ol>
+        </nav>
+      </div>
+      <!-- End Page Title -->
+
+      <section class="section dashboard">
+        <div class="row">
+          <!-- Left side columns -->
+          <div class="col-lg-8">
+            <div class="row">
+              <!-- Sales Card -->
+              <div class="col-xxl-4 col-md-6">
+                <div class="card info-card sales-card">
+                  <div class="card-body">
+                    <h5 class="card-title">Booking</h5>
+
+                    <div class="d-flex align-items-center">
+                      <div
+                        class="card-icon rounded-circle d-flex align-items-center justify-content-center"
+                      >
+                        <!-- <i class="bi bi-cart"></i> -->
+                      </div>
+                      <div class="ps-3">
+                        <h6>145</h6>
+                        <span class="text-success small pt-1 fw-bold">12%</span>
+                        <span class="text-muted small pt-2 ps-1">increase</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- End Sales Card -->
+            </div>
+          </div>
+          <!-- End Left side columns -->
+        </div>
+      </section>
+    </main>
+    <!-- End #main -->
+
+<?php loadPartition('admin-footer'); ?>

f25ad53bbc4488c29a2df3e4d55065d0fb5f4010
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2024-03-28 20:05:21 +0530

    Create router class

--- public/index.php
+++ public/index.php
@@ -5,5 +5,9 @@ require '../helpers.php';
 $routes = require basePath('routes.php');
 
 $uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+inspact($uri);
+inspact($method);
 
 require basePath('router.php');
--- router.php
+++ router.php
@@ -1,7 +1,65 @@
 <?php
 
-if(array_key_exists($uri, $routes)) {
-    require basePath($routes[$uri]);
-} else {
-    require basePath($routes['404']);
+class Router {
+    protected $routes = [];
+
+    /**
+     * Register Route
+     * 
+     * @param $method
+     * @param $uri
+     * @param $controller
+     * @return void
+     */
+    public function registerRoute($method, $uri, $controller) {
+        $this->routes = [
+            'method' => $method,
+            'uri' => $uri,
+            'controller' => $controller
+        ];
+    }
+
+    /**
+     * Add a GET route
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+    public function get($uri, $controller) {
+       $this->registerRoute('GET', $uri, $controller);
+    }
+
+    /**
+     * Add a POST route
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+    public function post($uri, $controller) {
+        $this->registerRoute('POST', $uri, $controller);
+    }
+
+    /**
+     * Add a PUT route
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+    public function put($uri, $controller) {
+        $this->registerRoute('PUT', $uri, $controller);
+    }
+
+    /**
+     * Add a DELETE route
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+    public function delete($uri, $controller) {
+        $this->registerRoute('DELETE', $uri, $controller);
+    }
 }
